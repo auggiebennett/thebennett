2a7b6c4ad5c6c076a14e0457d7dfd281194b4cff
Auggie Bennett <auggie@thebennett.net>
2024-03-12 17:06:44 -0500

    cleaning

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:the_bennett/screens/home/home_screen.dart';
-import 'package:the_bennett/screens/main/main_screen.dart';
+//import 'package:the_bennett/screens/main/main_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'constants.dart';
--- lib/models/Recommendation.dart
+++ lib/models/Recommendation.dart
@@ -1,4 +1,5 @@
-// ignore: file_names
+// ignore_for_file: file_names
+
 class Recommendation {
   final String? name, source, text;
 
